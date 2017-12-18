18224aabe9a5eb79966905174f1f79307fe06f71
Jussi Roos <jussi.roos@gmail.com>
2017-12-18 21:07:00 +0200

    Delete example.py

--- Sense-HAT/int_To_Pixels/example.py
+++
@@ -1,9 +0,0 @@
-# Exaple to print number 50 to Sense HAT modules screen on red color 
-
-from sense_hat import SenseHat
-from int_To_Pixels import intToPixels
-
-sense = SenseHat()
-
-# intToPixels(Intiger, foreground color as [R, G, B], background color as [R, G, B])
-sense.set_pixels(intToPixels(50, [255, 0, 0], [0, 0, 0]))
