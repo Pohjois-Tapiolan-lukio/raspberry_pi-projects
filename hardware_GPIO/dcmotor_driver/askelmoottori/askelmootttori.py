9d2821bca149e89b70d75819c3411f4140a01435
Matti Heikkinen <matti.heikkinen@pohjois-tapiola.com>
2018-02-01 09:26:47 +0000

    lisaa asekelmoottori

---
+++ hardware_GPIO/dcmotor_driver/askelmoottori/askelmootttori.py
@@ -0,0 +1,61 @@
+
+import RPi.GPIO as GPIO
+import time
+ 
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+coil_A_1_pin = 4 #  kytke askelmoottoriin
+coil_A_2_pin = 17 
+coil_B_1_pin = 23 
+coil_B_2_pin = 24 #
+#enable_pin   = 7 # joillaki moottoreilla
+ 
+
+StepCount = 8
+Seq = list(range(0, StepCount))
+Seq[0] = [0,1,0,0]
+Seq[1] = [0,1,0,1]
+Seq[2] = [0,0,0,1]
+Seq[3] = [1,0,0,1]
+Seq[4] = [1,0,0,0]
+Seq[5] = [1,0,1,0]
+Seq[6] = [0,0,1,0]
+Seq[7] = [0,1,1,0]
+ 
+#GPIO.setup(enable_pin, GPIO.OUT)
+GPIO.setup(coil_A_1_pin, GPIO.OUT)
+GPIO.setup(coil_A_2_pin, GPIO.OUT)
+GPIO.setup(coil_B_1_pin, GPIO.OUT)
+GPIO.setup(coil_B_2_pin, GPIO.OUT)
+ 
+#GPIO.output(enable_pin, 1)
+ 
+def setStep(w1, w2, w3, w4):
+    GPIO.output(coil_A_1_pin, w1)
+    GPIO.output(coil_A_2_pin, w2)
+    GPIO.output(coil_B_1_pin, w3)
+    GPIO.output(coil_B_2_pin, w4)
+ 
+def forward(delay, steps):
+    for i in range(steps):
+        for j in range(StepCount):
+            setStep(Seq[j][0], Seq[j][1], Seq[j][2], Seq[j][3])
+            time.sleep(delay)
+ 
+def backwards(delay, steps):
+    for i in range(steps):
+        for j in reversed(range(StepCount)):
+            setStep(Seq[j][0], Seq[j][1], Seq[j][2], Seq[j][3])
+            time.sleep(delay)
+            
+if __name__ == '__main__':
+    print("Testataan moottoria: ")
+    while True:
+        delay = raw_input("Haluttu viive: (ms)?")
+        steps = raw_input("Kuinka monta askelta eteenpain? ")
+        forward(int(delay) / 1000.0, int(steps))
+        steps = raw_input("Kuinka monta askelta taaksepain? ")
+        backwards(int(delay) / 1000.0, int(steps))
+
+
+
