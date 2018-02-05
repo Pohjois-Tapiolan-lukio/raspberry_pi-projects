e4452aee031af4454832807a5e158df45484743d
Niklas Halonen <kahvikannu@gmail.com>
2018-02-05 16:25:49 +0200

    Fix telegram bot python file

--- picamera/server.py
+++ picamera/server.py
@@ -3,11 +3,12 @@ import requests
 import picamera
 
 app = Flask(__name__)
+TOKEN = ''
 
 @app.route('/photo', methods=['GET','POST'])
 def handle_photo():
     camera_capture()
-    send_to_telegram()
+    send_to_telegram(TOKEN)
     return '{"success":"true"}'
 
 def camera_capture():
@@ -15,9 +16,11 @@ def camera_capture():
         with open('image.jpg', 'wb') as capture_file:
             camera.capture(capture_file)
 
-def send_to_telegram():
+def send_to_telegram(token=''):
+    if token == '':
+    	raise Exception('Telegram bot token not specified!')
     with open('image.jpg', 'rb') as capture_file:
-        url = 'https://api.telegram.org/bot<insert your token>/sendPhoto'
+        url = 'https://api.telegram.org/bot{}/sendPhoto'.format(token)
         f = {"photo": capture_file}
         p = {'chat_id': <your chat id>}
         res = requests.post(url, files=f, params=p)
