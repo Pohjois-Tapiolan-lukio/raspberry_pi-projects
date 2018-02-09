161ebdadeb0e2c69afb3887132d03f21961f5e26
Niklas Halonen <kahvikannu@gmail.com>
2018-02-09 16:59:45 +0000

    lisaa kytkenta

--- analog_MCP3008/saatovastus.py
+++ analog_MCP3008/saatovastus.py
@@ -1,3 +1,17 @@
+'''
+PINnit raspbyn ja dca muuntimen valissa
+Raspberry--> MCP3008
+Pin1 (3.3V)--> Pin16(VDD)
+Pin1 --> Pin15(vref)
+Pin6 (GND)--> Pin14(AGND)
+Pin23(Sclk)-->Pin13(CLK)
+Pin21(MISO)--> Pin12 (DOUT)
+Pin19(MOSI)--> Pin11 (DIN)
+Pin 24 (CE0) --> Pin10 (CS/SHDN)
+Pin6(GND)-->Pin9(DGND)
+
+output valilla 0...1
+'''
 from gpiozero import MCP3008
 import time
 
