f45de1aa879478b8d7ec1de4d12ef78e855a0522
Matti Heikkinen <matti.heikkinen@eduespoo.fi>
2018-04-24 04:49:16 -0700

    Add pwm control

--- hardware_GPIO/aiy-car-with-shield/shield-test/shield_test.py
+++ hardware_GPIO/aiy-car-with-shield/shield-test/shield_test.py
@@ -1,33 +1,28 @@
 import RPi.GPIO as GPIO
 import time
-from l293d import L293D
+from l293d_pwm import L293DPWM
 import threading 
  
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
  
-car = L293D(24,12,13,5)
+car = L293DPWM(24, 12, 26, 13, 5, 6)
 car.stop()
 
 
-PIN_LEFT = 26
-PIN_RIGHT = 6
-
-GPIO.setup(PIN_LEFT, GPIO.OUT)
-GPIO.setup(PIN_RIGHT, GPIO.OUT)
-
-
-PWM_MOTOR_LEFT = GPIO.PWM(PIN_LEFT, 50)
-PWM_MOTOR_RIGHT = GPIO.PWM(PIN_RIGHT, 50)
-
-PWM_MOTOR_LEFT.start(1)
-PWM_MOTOR_RIGHT.start(1)
-
-car.forward()
-PWM_MOTOR_LEFT.ChangeDutyCycle(100)
-PWM_MOTOR_RIGHT.ChangeDutyCycle(100)
+car.forward(100,100)
+print("forward")
 time.sleep(2)
-PWM_MOTOR_LEFT.ChangeDutyCycle(50)
-PWM_MOTOR_RIGHT.ChangeDutyCycle(50)
+car.stop()
+car.backward(100,100)
+print("reverse")
 time.sleep(2)
+car.stop()
+car.forward(50,100)
+print("left")
+time.sleep(2)
+car.forward(100, 50)
+print("right")
+time.sleep(2)
+
 car.stop()
