927856c2fb00b2472ecb29c54255b4a849b1ef34
Matti Heikkinen <matti.heikkinen@eduespoo.fi>
2018-04-25 17:04:52 -0700

    Calibrate car

--- hardware_GPIO/aiy-car-with-shield/shield-test/shield_test.py
+++ hardware_GPIO/aiy-car-with-shield/shield-test/shield_test.py
@@ -1,7 +1,6 @@
 import RPi.GPIO as GPIO
 import time
 from l293d_pwm import L293DPWM
-import threading 
  
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
@@ -10,19 +9,18 @@ car = L293DPWM(24, 12, 26, 13, 5, 6)
 car.stop()
 
 
-car.forward(100,100)
+car.forward(90, 100)
 print("forward")
 time.sleep(2)
 car.stop()
-car.backward(100,100)
+car.backward(90, 100)
 print("reverse")
 time.sleep(2)
 car.stop()
-car.forward(50,100)
+car.forward(25*0.9, 100)
 print("left")
 time.sleep(2)
-car.forward(100, 50)
+car.forward(90, 90*0.25)
 print("right")
 time.sleep(2)
-
 car.stop()
