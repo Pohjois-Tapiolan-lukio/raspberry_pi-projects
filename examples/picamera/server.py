e7390efdc4b7916fe059e335cc58d6603cd57c2e
Jens Pitkanen <jens@neon.moe>
2018-06-04 17:30:40 +0300

    Create picamera standalone example

---
+++ examples/picamera/server.py
@@ -0,0 +1,20 @@
+from flask import Flask
+import picamera
+
+app = Flask(__name__)
+
+@app.route('/')
+def handle_photo():
+    file = camera_capture()
+    return Response(file, mimetype="image/jpg")
+
+
+def camera_capture():
+    with picamera.PiCamera() as camera:
+        camera.capture("image.jpg")
+        with open('image.jpg', 'rb') as capture_file:
+            picture = capture_file.read()
+    return picture
+
+
+app.run(host="0.0.0.0")
