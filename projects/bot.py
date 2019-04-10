9a76244ff1ceca21a6939f03458aa55ce8702ae2
mxheikki <matti.heikkinen@eduespoo.fi>
2019-04-10 14:00:12 -0500

    Add emoji

--- projects/bot.py
+++ projects/bot.py
@@ -1,7 +1,8 @@
-#Installation:
+# Installation for rasberry pi controlled telegram bot
 # sudo pip install python-telegram-bot==12.0.0b1 --upgrade
-#Example script for Telegram bot
-MY_TOKEN ='insert your telegram token!'
+# sudo pip install emoji --upgrade
+
+MY_TOKEN ='ISERT YOUR TOKEN'
 
 
 from telegram.ext import Updater
@@ -11,6 +12,10 @@ dispatcher = updater.dispatcher
 
 import logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+
+from emoji import emojize
+cake = emojize("yummy :cake:", use_aliases=True)
+thumbsup = emojize(":thumbsup:", use_aliases=True)
 
 def start(update, context):
    context.bot.send_message(chat_id=update.message.chat_id, text="Olen Bottisi!")
@@ -31,6 +36,12 @@ dispatcher.add_handler(echo_handler)
 def reply(update, context):
     if update.message.text == 'K':
         context.bot.send_message(chat_id=update.message.chat_id, text='YES!')
+        context.bot.send_message(chat_id=update.message.chat_id, text=cake)
+        context.bot.send_message(chat_id=update.message.chat_id, text=thumbsup)
+    elif update.message.text == thumbsup:
+        context.bot.send_message(chat_id=update.message.chat_id, text='OK')
+        
+
 
 from telegram.ext import MessageHandler, Filters
 reply_handler = MessageHandler(Filters.text, reply)
